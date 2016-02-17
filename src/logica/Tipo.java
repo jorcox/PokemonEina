6491ec8dad109b3579a6e6eeef9466b92c37cf56
Javier Beltran <532581@unizar.es>
2016-02-17 21:03:16 +0100

    Creada enum Tipo (agua, fuego, etc) y MatrizEfectividad (x2, x1/2, etc).

---
+++ src/logica/Tipo.java
@@ -0,0 +1,17 @@
+package logica;
+
+/**
+ * 
+ * @author Javier Beltran, Jorge Cancer, Alejandro Dieste
+ * 
+ * Enumeracion de los tipos elementales del juego. Cada especie pokemon
+ * pertenece a uno o dos de estos tipos, y cada habilidad es de un tipo.
+ *
+ */
+public enum Tipo {
+	
+	ACERO, AGUA, BICHO, DRAGON, ELECTRICO, FANTASMA, FUEGO, 
+	HADA, HIELO, LUCHA, NORMAL, PLANTA, PSIQUICO, ROCA, 
+	SINIESTRO, TIERRA, VENENO, VOLADOR
+	
+}
